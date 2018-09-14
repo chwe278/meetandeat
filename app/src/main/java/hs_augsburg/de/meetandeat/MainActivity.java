da7eed95e5baa6fc8bb6e2fed8389627473dc5a0
Christian Weiland <christian_weiland@web.de>
2018-09-14 12:23:15 +0200

    homescreen edit

--- app/src/main/java/hs_augsburg/de/meetandeat/MainActivity.java
+++ app/src/main/java/hs_augsburg/de/meetandeat/MainActivity.java
@@ -1,7 +1,9 @@
 package hs_augsburg.de.meetandeat;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //test
+        Button btn_continue = findViewById(R.id.btn_continue);
+        btn_continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
 }

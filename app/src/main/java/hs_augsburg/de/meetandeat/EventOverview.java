9d1ff5f86a1bcf2b94c50b7ce6c31f0b70501fca
Christian Weiland <christian_weiland@web.de>
2018-09-15 12:02:45 +0200

    Add: Create Host Event Act. Event Overview Act.

---
+++ app/src/main/java/hs_augsburg/de/meetandeat/EventOverview.java
@@ -0,0 +1,13 @@
+package hs_augsburg.de.meetandeat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventOverview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_overview);
+    }
+}
--- app/src/main/java/hs_augsburg/de/meetandeat/HomeActivity.java
+++ app/src/main/java/hs_augsburg/de/meetandeat/HomeActivity.java
@@ -6,22 +6,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
 
-        Button btn_continue = findViewById(R.id.btn_continue);
-        btn_continue.setOnClickListener(new View.OnClickListener() {
+        Button btn_host = findViewById(R.id.btn_host);
+        btn_host.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            Intent startIntent = new Intent(getApplicationContext(),HomeActivity.class);
-            startActivity(startIntent);
+                Intent startIntent = new Intent(getApplicationContext(),CreateHEventActivity.class);
+                startActivity(startIntent);
             }
         });
-
-
     }
 }

301d3e3d96c2be6a63a8c486678bacf04ef907c8
michi1203 <43320093+michi1203@users.noreply.github.com>
2018-09-17 12:45:13 +0200

    guesthost

---
+++ app/src/main/java/hs_augsburg/de/meetandeat/CreateGuestEventActivity.java
@@ -0,0 +1,13 @@
+package hs_augsburg.de.meetandeat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateGuestEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_guest_event);
+    }
+}

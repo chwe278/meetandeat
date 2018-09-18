7c49e9bc7c43d45b053d6d428fc47b324c47b191
Christian Weiland <christian_weiland@web.de>
2018-09-18 12:53:28 +0200

    final header layout

--- app/src/main/java/hs_augsburg/de/meetandeat/CreateHostGuestActivity.java
+++ app/src/main/java/hs_augsburg/de/meetandeat/CreateHostGuestActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import java.util.ArrayList;
 
@@ -20,8 +21,12 @@ public class CreateHostGuestActivity extends AppCompatActivity implements View.O
 
         Button btn_host = findViewById(R.id.btn_host);
         Button btn_guest = findViewById(R.id.btn_guest);
+        ImageButton imgbtn_logo = findViewById(R.id.imgbtn_logo);
+        ImageButton imgbtn_name = findViewById(R.id.imgbtn_name);
         btn_host.setOnClickListener(this);
         btn_guest.setOnClickListener(this);
+        imgbtn_logo.setOnClickListener(this);
+        imgbtn_name.setOnClickListener(this);
     }
 
             public void onClick(View view) {
@@ -36,6 +41,14 @@ public class CreateHostGuestActivity extends AppCompatActivity implements View.O
                         sortOfEvent = 3;
                         Intent startIntent2 = new Intent(getApplicationContext(), CreateGuestEventActivity.class);
                         startActivity(startIntent2);
+                        break;
+                    case R.id.imgbtn_logo:
+                        Intent startIntent3 = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(startIntent3);
+                        break;
+                    case R.id.imgbtn_name:
+                        Intent startIntent4 = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(startIntent4);
                         break;
                 }
             }

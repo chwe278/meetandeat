092242858a065fc3aeeda97bcb49c9bac794ec7f
jsassembler <jonas.sonntag@hs-augsburg.de>
2018-09-18 12:44:07 +0200

    Basteln basteln

--- app/src/main/java/hs_augsburg/de/meetandeat/Host.java
+++ app/src/main/java/hs_augsburg/de/meetandeat/Host.java
@@ -1,6 +1,21 @@
 package hs_augsburg.de.meetandeat;
 
+import java.util.Date;
+
 public abstract class Host extends Event {
+
+    Date date;
+    String adress, title;
+    int ppl_amount;
+
+    public Host(int postcode, String city, String description, String tags, Date date, String adress, String title, int ppl_amount){
+        super(postcode, city, description, tags);
+        this.date = date;
+        this.adress = adress;
+        this.title = title;
+        this.ppl_amount = ppl_amount;
+
+    }
 
     //date, adress, ppl, title
 }
